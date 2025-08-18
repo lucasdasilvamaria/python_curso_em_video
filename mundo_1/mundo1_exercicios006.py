6a37734b969d52fec79f5e6c1e176b331c1d5243
lucasdasilvamaria <lucasdasilvamaria88@gmail.com>
2025-08-18 11:04:22 -0300

    Updated my studies

---
+++ mundo_1/mundo1_exercicios006.py
@@ -0,0 +1,7 @@
+# Desafio 5
+# Exercício Python #005 - Antecessor e Sucessor (https://youtu.be/664e0G_S9nU?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6)
+# Faça um programa que leia um número inteiro
+# e mostre na tela o seu dobro, triplo e raiz quadrada.
+
+num = float(input('Digite qualquer número: '))
+print(f"O dobro de {num} é {num*2}.\nO triplo de {num} é {num*3}.\nA raiz quadrada de {num} é {num**0.5}.")
