24a2352f13cf4fd18988b6e4b37aabd4549851d0
lucasdasilvamaria <lucasdasilvamaria88@gmail.com>
2025-08-18 10:59:56 -0300

    Updated my studies

---
+++ mundo_1/mundo1_exercicios005.py
@@ -0,0 +1,7 @@
+# Desafio 5
+# Exercício Python #005 - Antecessor e Sucessor (https://youtu.be/664e0G_S9nU?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6)
+# Faça um programa que leia um número inteiro
+# e mostre na tela o seu sucesso e seu antecessor.
+
+num = float(input('Digite qualquer número: '))
+print(f"O antecessor de {num} é {num-1}. O sucessor de {num} é {num+1}.")
