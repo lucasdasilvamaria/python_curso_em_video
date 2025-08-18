114922827552f3f5be2b42c597028fb48127a6a7
lucasdasilvamaria <lucasdasilvamaria88@gmail.com>
2025-08-18 17:06:27 -0300

    Updated my studies

---
+++ mundo_1/mundo1_exercicios0010.py
@@ -0,0 +1,9 @@
+# Exercício Python #010 - Conversor de Moedas
+#
+# https://youtu.be/yO9CI_B7XiU
+#
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
+# e mostre quantos dólares ela pode comprar.
+
+m = float(input('Digite quanto dinheiro você tem em reais (R$): '))
+print(f"Você pode comprar {m/5.6:.2f} dólares (USD).")
